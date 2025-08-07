ada4dac03e3241941c5d0295d9dd2910dcfa540e
alejandrons <anoriega@unal.edu.co>
2025-08-07 01:06:32 -0500

    Third challenge solved. Endpoint functional

---
+++ parte3_webhook/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+webhook_logs = []
+
+app = FastAPI()
+
+@app.post("/webhook")
+def receive_data(event:str, message:str, timestamp:str):
+    webhook_logs.append({
+        "evento": event,
+        "mensaje": message,
+        "timestamp": timestamp
+    })
+
+    return {
+        "status": "ok",
+        "recibido": webhook_logs[-1]
+    }
