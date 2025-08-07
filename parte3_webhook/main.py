982dede8f6d0b17ca52fc3dd934e3148337e8b6d
alejandrons <anoriega@unal.edu.co>
2025-08-07 02:31:00 -0500

    Creating use examples and removing usless control prompts

--- parte3_webhook/main.py
+++ parte3_webhook/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 webhook_logs = []
 
 app = FastAPI()
+app.title = "Bayma Notification Center"
 
 @app.post("/webhook")
 def receive_data(event:str, message:str, timestamp:str):
